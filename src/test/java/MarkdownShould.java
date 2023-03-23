f1f56fbfec1c334b9f33c36cf21e77e1912442a2
Test Carlos <carlosmonroy2908@gmail.com>
2023-03-23 15:43:46 +0100

    Initial setup

---
+++ src/test/java/MarkdownShould.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class MarkdownShould {
+
+    @Test
+    public void foo(){
+        Assertions.assertEquals("foo", "foo");
+    }
+}
